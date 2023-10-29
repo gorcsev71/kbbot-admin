8992b003e7421bafa45e5969c7d7a36a2f6c624f
gorcsev71 <gorcsev71@gmail.com>
2023-10-29 22:12:34 +0100

    modifications for Azure deployment

--- kbbotdjango/settings.py
+++ kbbotdjango/settings.py
@@ -48,7 +48,8 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 
 # Application definition

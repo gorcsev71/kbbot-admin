917ef0816574bf7b558ccef8ed91a848b71ca2ef
gorcsev71 <gorcsev71@gmail.com>
2023-10-30 18:15:09 +0100

    configuring production settings

--- kbbotdjango/settings.py
+++ kbbotdjango/settings.py
@@ -39,18 +39,15 @@ AZURE_STORAGE_ACCOUNT_NAME = env('AZURE_STORAGE_ACCOUNT_NAME')
 AZURE_STORAGE_BLOB_CONTAINER_NAME = env('AZURE_STORAGE_BLOB_CONTAINER_NAME')
 AZURE_STORAGE_ACCESS_KEY = env('AZURE_STORAGE_ACCESS_KEY')
 
-# False if not in os.environ because of casting above
-DEBUG = env('DEBUG')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-if not DEBUG:
-    ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -99,6 +96,8 @@ WSGI_APPLICATION = 'kbbotdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -141,8 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static_collected'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- kbbotdjango/wsgi.py
+++ kbbotdjango/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kbbotdjango.settings')
+# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# on Azure App Service and should use the production settings in production.py.
+settings_module = "kbbotdjango.production" if 'WEBSITE_HOSTNAME' in os.environ else 'kbbotdjango.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
 
 application = get_wsgi_application()
